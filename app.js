f175793f0aad8be9de23fcd2797d91b9892758be
abdulloh055 <83707118+abdulloh055@users.noreply.github.com>
2021-05-28 17:17:24 +0500

    Fixed bags

--- app.js
+++ app.js
@@ -1,74 +1,58 @@
-const userInput = document.getElementById('userInput')
-const enter = document.getElementById('enter')
-const ul = document.querySelector('ul')
+const userInput = document.getElementById("userInput");
+const enter = document.getElementById("enter");
+const ul = document.querySelector("ul");
+const li = document.querySelectorAll("li");
 
-const li = document.querySelectorAll('li')
-
-function userInputLength () 
-{
-    return userInput.Value.length
+function userInputLength() {
+  return userInput.value.length;
 }
 
-
-function listslength () {
- return li.length
+function listslength() {
+  return li.length;
 }
-   
-    
-    function createListItems() {
-        const li = document.createElement('li')
-        li.classList.add('done2')
-        li.appendChild(document.createTextNode(userInput.value))
-        ul.appendChild(li)
-        userInput.value = ''
 
+function createListItems() {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(userInput.value));
+  ul.appendChild(li);
+  userInput.value = "";
 
-        function crossOut() {
-            // if(li.classList.contains('done2')) {
-            //     li.classList.add('done')
-            //     li.classList.remove('done2')
-            // } else if(!li.classList.contains('done2')) {
-            //     li.classList.add('done2')
-            //     li.classList.remove('done')
+  function crossOut() {
+    // if(li.classList.contains('done2')) {
+    //     li.classList.add('done')
+    //     li.classList.remove('done2')
+    // } else if(!li.classList.contains('done2')) {
+    //     li.classList.add('done2')
+    //     li.classList.remove('done')
 
-            // } 
+    // }
 
-            li.classList.toggle('done')
-        }
-        li.addEventListener('click', crossOut)
+    li.classList.toggle("done");
+  }
+  li.addEventListener("click", crossOut);
 
-        const deleteBtn = document.createElement('button')
-        deleteBtn.appendChild(document.createTextNode('X'))
-        li.appendChild(deleteBtn)
-        deleteBtn.addEventListener('click', deleteListItems)
+  const deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("X"));
+  li.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", deleteListItems);
 
-        function deleteListItems() {
-            li.classList.add('delete')
-        }
-    }
+  function deleteListItems() {
+    li.classList.add("delete");
+  }
+}
 
 function addListAfterClick() {
-        if (userInputLength() > 0) {
-            createListElement()
-        }
-    }
-    
-    function addListAfterKeypress(e) {
-        if (userInputLength() > 0 && e.which === 13) {
-            createListElement()
-        }
-    }
+  if (userInputLength() > 0) {
+    createListItems();
+  }
+}
 
-    
+function addListAfterKeypress(event) {
+  if (userInputLength() > 0 && event.keyCode === 13) {
+    createListItems();
+  }
+}
 
-    enter.addEventListener('click', addListAfterClick)
+enter.addEventListener("click", addListAfterClick);
 
-    userInput.addEventListener('keypress', addListAfterKeypress)
-
-
-
-
-
-
-
-    
+userInput.addEventListener("keypress", addListAfterKeypress);
